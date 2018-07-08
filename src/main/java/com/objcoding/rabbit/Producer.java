e62e7c0bfaf224b964fa16b38365d3872d57494c
zch <zhangchenghui.dev@gmail.com>
2018-07-09 01:52:51 +0800

    add scheduled prouce method

--- src/main/java/com/objcoding/rabbit/Producer.java
+++ src/main/java/com/objcoding/rabbit/Producer.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author zhangchenghui.dev@gmail.com
@@ -30,9 +31,14 @@ public class Producer {
         return outputChannel.output();
     }
 
-
     public void produce(String playload) {
         log.info("produce: {}", playload);
         getMessageChannel().send(MessageBuilder.withPayload(playload).build());
+    }
+
+    @Scheduled(initialDelay = 5000, fixedRate = 5000)
+    public void scheduledProuce() {
+        log.info("=== produce ===");
+        getMessageChannel().send(MessageBuilder.withPayload("=== scheduled ===").build());
     }
 }
